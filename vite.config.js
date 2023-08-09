de9231e45610231dbbb9a0e5a000cccbf01f7ac6
Kelvin <kel7.dev@gmail.com>
2023-08-08 23:16:11 -0400

    adding folder

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/price-component/",
   server: {
     port: 3000,
   },
