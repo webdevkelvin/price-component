eec94e737ef2ad84c451a59b7989bc56e268a6a7
Kelvin <kel7.dev@gmail.com>
2023-08-08 23:29:39 -0400

    base updated

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/price-component/",
+  base: "https://webdevkelvin.github.io/price-component/",
   server: {
     port: 3000,
   },
